6a1592f011ecf8a12b38a36ba55a9625c51d819d
admin <andridenis@gmail.com>
2019-04-26 14:01:28 +0200

    colors disabled

--- Versuch 1/versuch1/plotStl.m
+++ Versuch 1/versuch1/plotStl.m
@@ -1,19 +1,19 @@
 %% Skript to plot .stl file in octave 
 
-%filename='zylinder.stl'; % .stl file 
+filename='zylinder.stl'; % .stl file 
 %filename='w210.stl';
 %filename='coaxial.stl'; % .stl file
 %filename='cut.stl'; % .stl file
 %filename='cube.stl'; % .stl file
 %filename='kugel.stl'; % .stl file
 %filename='kugel2.stl';
-filename='microstrip.stl';
+%filename='microstrip.stl';
 %%reading data from .stl file and converting to octave  
 [X,Y,Z,normals,stlname]=read_stl(filename);   
 %%patching stuff together and visualising
 figure(1)
 
-patch(X,Y,Z,normals);
+patch(X,Y,Z,zeros(3,columns(X)));
 title(filename);
 view([1,-1,0.5])    
 xlabel('x')
