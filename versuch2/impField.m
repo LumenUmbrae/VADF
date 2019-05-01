9fe5537108ecc0f457ba01e86094c901fbaefed6
Marc <marc.bodem@stud.tu-darmstadt.de>
2019-05-01 18:00:11 +0200

    BilderUndCode_9_10_noch_nicht_korrekt

--- versuch2/impField.m
+++ versuch2/impField.m
@@ -30,18 +30,31 @@ for i=1:nx
     for j=1:ny
         for k=1:nz
             % kanonischen Index n bestimmen
-            n = 
+            n = k*j*i;
             
             % x-, y- und z-Koordinate des Gitterpunktes bestimmen
             x = xmesh(i);
             y = ymesh(j);
             z = zmesh(k);
-
+            
+            x2 = xmesh(i+1);
+            y2 = ymesh(j+1);
+            if (k)>=rows(zmesh)+1
+              z2=z;
+            else
+            z2 = zmesh(k+1);
+            endif
+            vectors=field(x+(x2-x)/2,y+(y2-y)/2,z+(z2-z)/2);
+            xvec=vectors(1);
+            yvec=vectors(2);
+            zvec=vectors(3);
             % Bogenwert für x-Kante mit Index n
+            fieldbow(i) = xvec.*(x2-x);
             
             % Bogenwert für y-Kante mit Index n
-            
+            fieldbow(j) = yvec.*(y2-y);
             % Bogenwert für z-Kante mit Index n
+            fieldbow(k) = zvec.*(z2-z);
         end
     end
 end
--- versuch2/plotImpField.m
+++ versuch2/plotImpField.m
@@ -6,17 +6,38 @@
 
 %% Aufgabe 9
 % Beispielgitter erzeugen (3D)
-
+nxy=2
+    xmesh=1:1:nxy;
+    for i=1:1:2*nxy-1
+      xmesh=[xmesh, 1:1:nxy];
+      xmesh
+    endfor
+    
+    ymesh=ones(nxy, 1)';
+    
+    for i=2:1:nxy
+       ymesh=[ymesh, i.*ones(nxy, 1)'];
+    endfor
+    ymesh=[ymesh,ymesh];
+    
+    zmesh = [];
+    for i=0:1:nxy-1
+      zmesh = [zmesh, i*ones(nxy*nxy,1)'];
+    endfor
+    zmesh
+    a=columns(xmesh)
+    a2=columns(ymesh)
+    b=columns(zmesh)
 %% msh struct berechnen
 msh = cartMesh(xmesh, ymesh, zmesh);
 
 %% anonymous function der beiden gegebenen Felder bestimmen
-% f1 = 
+ f1 = @(x,y,z)([1./x.^2, 0.01*x, y+z]);
 % f2 = 
 
 %% Bogengröße der beiden Felder mithilfe von impField bestimmen
 fbow1 = impField( msh, f1 );
-fbow2 = impField( msh, f2 );
+%fbow2 = impField( msh, f2 );
 
 %% diskretes Feld fbow mithilfe von plotArrowfield plotten
 figure;
