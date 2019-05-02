3dcd4b54f1983fe6f4b17ff49e888ff7b72940a0
admin <andridenis@gmail.com>
2019-05-02 15:46:30 +0200

    Fehler

--- versuch2/plotMesh.m
+++ versuch2/plotMesh.m
@@ -9,23 +9,38 @@ function [  ] = plotMesh( msh )
   
 
 % Zuweisen von nx, nz, nz, xmesh, ymesh und zmesh
-nx=mesh.nx;
-ny=mesh.ny;
-nz=mesh.nz;
-xmesh=mesh.xmesh;
-ymesh=mesh.ymesh;
-zmesh=mesh.zmesh;
+nx=msh.nx;
+ny=msh.ny;
+nz=msh.nz;
+xmesh=msh.xmesh;
+ymesh=msh.ymesh;
+zmesh=msh.zmesh;
 
 % Zeichnen aller Kanten mithilfe einer Dreifachschleife
 figure;
 for i=1:nx
     for j=1:ny
         for k=1:nz
+		point=[xmesh(i);ymesh(j);zmesh(k)];
             % x-Kante zeichnen, falls keine Geisterkante vorliegt
-            
+            if i!=nx
+              pointx=point;
+              pointx(1)=xmesh(i+1);
+              line(point,pointx);
+            endif
+		
             % y-Kante zeichnen, falls keine Geisterkante vorliegt
-            
+            if i!=ny
+              pointy=point;
+              pointy(2)=ymesh(j+1);
+              line(point,pointy);
+            endif
             % z-Kante zeichnen, falls keine Geisterkante vorliegt
+            if i!=nz
+              pointz=point;
+              pointz(1)=zmesh(k+1);
+              line(point,pointz);
+            endif
         end
     end
 end
