c0ef49780988b5f14a692f25f9c8f74fc36ebc89
admin <andridenis@gmail.com>
2019-05-02 16:12:29 +0200

    all

--- versuch2/plotMesh.m
+++ versuch2/plotMesh.m
@@ -26,20 +26,20 @@ for i=1:nx
             if i!=nx
               pointx=point;
               pointx(1)=xmesh(i+1);
-              line(point,pointx);
+              line([point(1),pointx(1)],[point(2),pointx(2)],[point(3),pointx(3)]);
             endif
 		
             % y-Kante zeichnen, falls keine Geisterkante vorliegt
-            if i!=ny
+            if j!=ny
               pointy=point;
               pointy(2)=ymesh(j+1);
-              line(point,pointy);
+              line([point(1),pointy(1)],[point(2),pointy(2)],[point(3),pointy(3)]);
             endif
             % z-Kante zeichnen, falls keine Geisterkante vorliegt
-            if i!=nz
+            if k!=nz
               pointz=point;
-              pointz(1)=zmesh(k+1);
-              line(point,pointz);
+              pointz(3)=zmesh(k+1);
+               line([point(1),pointz(1)],[point(2),pointz(2)],[point(3),pointz(3)]);
             endif
         end
     end
