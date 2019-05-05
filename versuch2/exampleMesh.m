8e55f3044ffa4fe035eb7d818ef9d8a3ffe1b7a1
Unknown <andridenis@gmail.com>
2019-05-05 21:31:57 +0200

    Codes für mesh

---
+++ versuch2/exampleMesh.m
@@ -0,0 +1,5 @@
+xmesh=[1,4,7];
+ymesh=[1,3,7,8];
+zmesh=[1,2,5,6,8];
+msh=cartMesh(xmesh,ymesh,zmesh);
+plotMesh(msh);
--- versuch2/plotMesh.m
+++ versuch2/plotMesh.m
@@ -9,23 +9,38 @@ function [  ] = plotMesh( msh )
   
 
 % Zuweisen von nx, nz, nz, xmesh, ymesh und zmesh
-nx=mesh.nx;
-ny=mesh.ny;
-nz=mesh.nz;
-xmesh=mesh.xmesh;
-ymesh=mesh.ymesh;
-zmesh=mesh.zmesh;
+nx=msh.nx;
+ny=msh.ny;
+nz=msh.nz;
+xmesh=msh.xmesh;
+ymesh=msh.ymesh;
+zmesh=msh.zmesh;
 
 % Zeichnen aller Kanten mithilfe einer Dreifachschleife
 figure;
 for i=1:nx
     for j=1:ny
         for k=1:nz
+		point=[xmesh(i);ymesh(j);zmesh(k)];
             % x-Kante zeichnen, falls keine Geisterkante vorliegt
-            
+            if i!=nx
+              pointx=point;
+              pointx(1)=xmesh(i+1);
+              line([point(1),pointx(1)],[point(2),pointx(2)],[point(3),pointx(3)]);
+            endif
+		
             % y-Kante zeichnen, falls keine Geisterkante vorliegt
-            
+            if j!=ny
+              pointy=point;
+              pointy(2)=ymesh(j+1);
+              line([point(1),pointy(1)],[point(2),pointy(2)],[point(3),pointy(3)]);
+            endif
             % z-Kante zeichnen, falls keine Geisterkante vorliegt
+            if k!=nz
+              pointz=point;
+              pointz(3)=zmesh(k+1);
+               line([point(1),pointz(1)],[point(2),pointz(2)],[point(3),pointz(3)]);
+            endif
         end
     end
 end
