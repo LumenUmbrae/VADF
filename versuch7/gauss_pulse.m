59df6a71bc3339c4165c564bc608170e4e6a0e0f
Marc <marc.bodem@stud.tu-darmstadt.de>
2019-06-13 16:08:27 +0200

    läuft noch nicht GAuss

--- versuch7/gauss_pulse.m
+++ versuch7/gauss_pulse.m
@@ -2,7 +2,7 @@ function [jvalue] = gauss_pulse(t, fmax)
 
 % value
 omega = 2*pi*fmax;
-sigma = sqrt(ln(10))/(pi*fmax);
-tm = t-.sqrt(6*sigma*ln(10));
+sigma = sqrt(log(10))/(pi*fmax);
+tm = t%.-sqrt(6*sigma*log(10));
 jvalue = exp(-tm.^2 ./ (2*sigma^2));
 end
