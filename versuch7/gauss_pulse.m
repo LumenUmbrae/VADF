e70a75aaf7529b5dde3dbfda50d2a2b3d17a4052
admin <andridenis@gmail.com>
2019-06-13 15:07:04 +0200

    pulses

--- versuch7/gauss_pulse.m
+++ versuch7/gauss_pulse.m
@@ -1,7 +1,8 @@
 function [jvalue] = gauss_pulse(t, fmax)
 
 % value
-omega = 
-sigma = 
-tm = 
+omega = 2*pi*fmax;
+sigma = sqrt(ln(10))/(pi*fmax);
+tm = t-.sqrt(6*sigma*ln(10));
 jvalue = exp(-tm.^2 ./ (2*sigma^2));
+end
